480af1416870ea3a1c5784cef0f83abaee365759
qq7 <qq7dev@gmail.com>
2020-09-07 20:46:10 +0000

    update redmine to v16

--- overlay/usr/lib/inithooks/bin/redmine.py
+++ overlay/usr/lib/inithooks/bin/redmine.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Set Redmine admin password and email
 
 Option:
@@ -19,16 +19,16 @@ from mysqlconf import MySQL
 
 def usage(s=None):
     if s:
-        print >> sys.stderr, "Error:", s
-    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
-    print >> sys.stderr, __doc__
+        print("Error:", s, file=sys.stderr)
+    print("Syntax: %s [options]" % sys.argv[0], file=sys.stderr)
+    print(__doc__, file=sys.stderr)
     sys.exit(1)
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
                                        ['help', 'pass=', 'email='])
-    except getopt.GetoptError, e:
+    except getopt.GetoptError as e:
         usage(e)
 
     password = ""
@@ -57,9 +57,10 @@ def main():
             "admin@example.com")
 
     inithooks_cache.write('APP_EMAIL', email)
-    
-    salt = "".join(random.choice(string.letters) for line in range(16))
-    hashpass = hashlib.sha1(salt + hashlib.sha1(password).hexdigest()).hexdigest()
+
+    salt = "".join(random.choice(string.ascii_letters) for line in range(16))
+    pass_bytes = (salt + str(hashlib.sha1(password.encode('utf-8')))).encode('utf-8')
+    hashpass = hashlib.sha1(pass_bytes).hexdigest()
     user_id = 1
 
     m = MySQL()
